3fb15d1c6b6bf27677dc2ed305067eb3bfcc0903
returnkeikaku <returnkeikaku@gmail.com>
2021-04-28 11:21:21 +0900

    Google Analytics トラッキングコード追加

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,16 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-195711162-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-195711162-1');
+    </script>
+
     <!--説明文-->
     <meta name="description" content="ひきこもりの居場所として当事者向けに設置した掲示板です。
         他愛のない話題でも悩み事でもなんでもかまいませんのでどうぞご利用ください。
